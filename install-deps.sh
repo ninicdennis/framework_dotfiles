961cca80f93c8e7d4a3b82af7958ea560af96942
ninicdennis <pupperbot@protonmail.com>
2025-11-28 10:31:34 -0700

    feat: :sparkles: Add some extra features to zshrc!

--- install-deps.sh
+++ install-deps.sh
@@ -40,6 +40,38 @@ else
   echo "oh-my-zsh is already installed"
 fi
 
+# Install zsh-autosuggestions plugin
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+  echo "Installing zsh-autosuggestions..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+else
+  echo "zsh-autosuggestions is already installed"
+fi
+
+# Install zsh-syntax-highlighting plugin
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+  echo "Installing zsh-syntax-highlighting..."
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+else
+  echo "zsh-syntax-highlighting is already installed"
+fi
+
+# Install zsh-you-should-use plugin
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/you-should-use" ]; then
+  echo "Installing zsh-you-should-use..."
+  git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/you-should-use
+else
+  echo "zsh-you-should-use is already installed"
+fi
+
+# Install zsh-history-substring-search plugin
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-history-substring-search" ]; then
+  echo "Installing zsh-history-substring-search..."
+  git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+else
+  echo "zsh-history-substring-search is already installed"
+fi
+
 echo ""
 echo "All dependencies installed successfully!"
 echo "Run ./stow.sh to apply the dotfiles."
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -70,7 +70,7 @@ ZSH_THEME="agnoster"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting you-should-use sudo z history-substring-search command-not-found)
 
 source $ZSH/oh-my-zsh.sh
 
